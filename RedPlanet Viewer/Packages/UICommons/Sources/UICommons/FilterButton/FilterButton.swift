f33f747dd4eb52411e0c1ecfc7d659ac0a4beb65
Oleksandr Zavazhenko <zavazhenski@gmail.com>
2023-12-18 19:48:43 +0100

    Implement FilterButton

---
+++ RedPlanet Viewer/Packages/UICommons/Sources/UICommons/FilterButton/FilterButton.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+public enum FilterButtonType {
+    case rover, camera
+}
+
+public struct FilterButton: View {
+    private let type: FilterButtonType
+    private let selectedValue: String?
+    private let onTapAction: () -> Void
+
+    public init(
+        type: FilterButtonType,
+        selectedValue: String? = nil,
+        onTapAction: @escaping () -> Void
+    ) {
+        self.type = type
+        self.selectedValue = selectedValue
+        self.onTapAction = onTapAction
+    }
+
+    public var body: some View {
+        buildContent()
+    }
+
+    private func buildContent() -> some View {
+        HStack(
+            alignment: .center,
+            spacing: 6.0
+        ) {
+            Image(named: type == .camera ? .cameraIcon : .roverIcon)
+                .resizable()
+                .frame(width: 24.0, height: 24.0)
+
+            Text(selectedValue ?? "All")
+                .font(.system(size: 17, weight: .bold))
+
+            Spacer()
+        }
+        .padding(7.0)
+        .background(
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(Color.backgroundOne)
+                .shadow(
+                    color: .layerOne.opacity(0.1),
+                    radius: 5,
+                    x: 0, y: 4
+                )
+        )
+        .frame(maxWidth: 140.0)
+        .onTapGesture {
+            onTapAction()
+        }
+    }
+}
+
+#Preview {
+    Group {
+        FilterButton(type: .camera, onTapAction: {})
+        FilterButton(type: .rover, selectedValue: "Curiosity", onTapAction: { print("Hello world")})
+    }
+}
